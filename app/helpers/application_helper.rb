28a75a396a529be274239d733729cabac5d609b7
Simon Wallner <me@simonwallner.at>
2013-05-08 11:58:51 +0200

    added explicit :email renderer to autolink in the markup parser. should be refactored in explicit and tested funciton at some point.
    fixes #1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,26 +21,39 @@ module ApplicationHelper
 			"<a href=\"#{link}\">#{alt_text}</a>"
 		end
 
+		# parse links and email addresses even if they are not enclosed
+		# in ()
+		# cut down long links to make them prettier
 		def autolink(link, link_type)
-			if link[-1] =='/'
-				link=link[0..-1]
-			end
-			link_title= link.split("/")
-			link_end =""
-			if link_title.length>5
-				link_title=link_title[0..4]
-				link_end = "/..."
-			end
-			link_t = link_title[2]
-			for i in 3..link_title.length-1
-				if link_title[i].length>20
-					link_end = "/..."
-					break
+			if (link_type == :url)
+				if link[-1] =='/'
+					link = link[0..-1]
 				end
-				link_t += "/" + link_title[i]
+			
+				link_title = link.split("/")
+				link_end = ""
+			
+				if link_title.length > 5
+					link_title=link_title[0..4]
+					link_end = "/..."
+				end
+			
+				link_t = link_title[2]
+			
+				for i in 3..link_title.length-1
+					if link_title[i].length>20
+						link_end = "/..."
+						break
+					end
+					link_t += "/" + link_title[i]
+				end
+				link_t +=link_end
+				return "<a href=\"#{link}\">#{link_t}</a>"
+			elsif (link_type == :email)
+				return "<a href=mailto:\"#{link}\">#{link}</a>"
+			else
+				return nil
 			end
-			link_t +=link_end
-			"<a href=\"#{link}\">#{link_t}</a>"
 		end
 	end
 
